d8908f9d407e0b75e08e8b20083a82306fe3d8d0
Fadli Zainuddin 202102266 <fiqrim775@gmail.com>
2023-06-07 10:03:55 +0700

    Initial commit

---
+++ app/src/main/java/com/fadlizainuddin/recyclerviewb/MahasiswaModel.java
@@ -0,0 +1,41 @@
+package com.fadlizainuddin.recyclerviewb;
+
+public class MahasiswaModel {
+    private String NIM, Nama, jenisKelamin, JP;
+
+    public MahasiswaModel(){
+
+    }
+
+    public String getNIM() {
+        return NIM;
+    }
+
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getJP() {
+        return JP;
+    }
+
+    public void setJP(String JP) {
+        this.JP = JP;
+    }
+}
